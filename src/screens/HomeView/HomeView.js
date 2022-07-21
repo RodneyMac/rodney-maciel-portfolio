32a094577f1f11cfd3d5a6a015d0a367e477c62d
RodneyMac <rodneydavidmaciel@gmail.com>
2022-07-21 00:14:36 -0300

    HomeView screen and Navbar component added

---
+++ src/screens/HomeView/HomeView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import "../HomeView/HomeView.css";
+
+const HomeView = () => {
+  return (
+    <div className='container'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default HomeView;
