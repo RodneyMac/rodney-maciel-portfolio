54cf175ef990e6415c009955fd1cd610f29b9a2c
RodneyMac <rodneydavidmaciel@gmail.com>
2022-07-21 15:25:35 -0300

    Navbar styles, icon and routes added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import "../Navbar/Navbar.css";
+import {Link} from "react-router-dom";
+import logoImage from "../../assets/logo/rod.png";
 
 const Navbar = () => {
   return (
-    <div className='navbar-container'>Navbar</div>
+    <div className='navbar-container'>
+      <div className='navbar-content'>
+        <div className='div-logo'>
+          <img src={logoImage} alt="Logo" className='logo'/>
+        </div>
+        <ul className='nav-links'>
+          <Link to="/" className='link-router'><li className='nav-link'>About</li></Link>
+          <Link to="/skills" className='link-router'><li className='nav-link'>Skills</li></Link>
+          <Link to="/works" className='link-router'><li className='nav-link'>Works</li></Link>
+          <Link to="/contact" className='link-router'><li className='nav-link'>Contact</li></Link>
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- src/screens/HomeView/HomeView.js
+++ src/screens/HomeView/HomeView.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import Footer from "../../components/Footer/Footer";
 import "../HomeView/HomeView.css";
+import About from "../../components/About/About";
+import Skills from "../../components/Skills/Skills";
+import Works from "../../components/Works/Works";
+import Contact from "../../components/Contact/Contact";
+import {Routes, Route} from "react-router-dom";
 
 const HomeView = () => {
   return (
     <div className='container'>
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<About/>}/>
+        <Route path="/skills" element={<Skills/>}/>
+        <Route path="/works" element={<Works/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+      <Footer/>
     </div>
   )
 }
